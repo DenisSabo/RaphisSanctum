a1fac79423677abf5f3244915bebd3318787b757
Denis Sabolotni <denissabolotnimail.com>
2018-05-31 15:25:24 +0200

    Deleted redundant directory

--- Assignment02/src/main/java/fh/vv/assignment02/restful/ContractRepository.java
+++
@@ -1,9 +0,0 @@
-package fh.vv.assignment02.restful;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface ContractRepository extends CrudRepository<Contract, Long>{
-}
