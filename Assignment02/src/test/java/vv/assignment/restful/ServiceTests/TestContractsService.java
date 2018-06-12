5826fadca57fd1038ad5554a3d523c487391799f
Denis Sabolotni <denissabolotnimail.com>
2018-06-12 16:22:26 +0200

    Implemented test for lost update problem and improved already existing test

--- Assignment02/src/test/java/vv/assignment/restful/ServiceTests/TestContractsService.java
+++ Assignment02/src/test/java/vv/assignment/restful/ServiceTests/TestContractsService.java
@@ -1,111 +1,159 @@
 package vv.assignment.restful.ServiceTests;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 import vv.assignment.restful.Contract.Contract;
+import vv.assignment.restful.Contract.ContractService;
 import vv.assignment.restful.MyExceptions.ServerNotTunedOnRequestException;
-import vv.assignment.restful.Proxy.LocalCallConstants;
+import vv.assignment.restful.Proxy.ContractManagement;
+import vv.assignment.restful.Proxy.LocalRequestsUtil;
 
 import java.math.BigDecimal;
-import java.net.URI;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static vv.assignment.restful.Contract.Contract.AllowedContracts.KFZ;
 import static vv.assignment.restful.Contract.Contract.AllowedContracts.KRANKENVERSICHERUNG;
-import static vv.assignment.restful.Proxy.LocalCallConstants.*;
+import static vv.assignment.restful.Proxy.LocalRequestsUtil.deleteTestUser;
 
+
+
+@WebMvcTest(ContractService.class) // The only thing we want to test in this JUNIT test is the Contract Service
 public class TestContractsService {
-    // This restTemplate uses a predefined User for basic authentication
-    static RestTemplate restTemplate = getAuthenticatedRestTemplate();
+
+    /**
+     * The proxy provides the possibility to make requests to the "Contract-Endpoints" easily
+     */
+    ContractManagement proxy = new ContractManagement();
+
+
+    // local contracts that can be used in tests
+    Contract healthInsurance;
+    Contract kfz;
+
 
     /**
      * Creates a User, that can be used for authentication by the test cases
      */
     @BeforeAll
     public static void createTestUser() throws ServerNotTunedOnRequestException {
-        LocalCallConstants.createTestUser();
+        LocalRequestsUtil.createTestUser();
     }
 
+
+
+    /**
+     * Deletes test user
+     */
     @AfterAll
     public static void cleanUp(){
         deleteTestUser();
     }
 
+
+
+    @BeforeEach
+    public void reinitializeContracts(){
+        healthInsurance = new Contract(KRANKENVERSICHERUNG, new BigDecimal("416.45"));
+        kfz = new Contract(KFZ, new BigDecimal("42.42"));
+    }
+
+
+    /**
+     * Clears database from all contracts
+     */
+    @AfterEach
+    public void deleteContracts() {
+        proxy.deleteAll();
+    }
+
+
+
+    /**
+     * Tests if application creates and deletes contracts correctly
+     */
     @Test
     public void createAndDeleteContracts(){
-        /**
-         * Create new Contracts
-         */
-        Contract healthInsurance = new Contract(KRANKENVERSICHERUNG, new BigDecimal("416.45"));
-        ResponseEntity<Void> locationToContract1 = createContract(healthInsurance);
-        assertThat(locationToContract1.getStatusCode(), equalTo(HttpStatus.CREATED));
 
-        Contract anotherHealthInsurance = new Contract(KRANKENVERSICHERUNG, new BigDecimal("600.00"));
-        ResponseEntity<Void> locationToContract2 = createContract(anotherHealthInsurance);
-        assertThat(locationToContract2.getStatusCode(), equalTo(HttpStatus.CREATED));
-        /**
-         * Try to get created Contracts
-         */
-        ResponseEntity<Contract> healthInsurance1FromDatabase =
-                getContract(locationToContract1.getHeaders().getLocation());
-        assertThat(healthInsurance1FromDatabase.getStatusCode(), equalTo(HttpStatus.OK));
-        // Compare local health insurance with health insurance from database, gained through get request
-        assertThat(healthInsurance1FromDatabase.getBody().getKindOfContract(),
-                equalTo(healthInsurance.getKindOfContract()));
-        assertThat(healthInsurance1FromDatabase.getBody().getYearlyFee(),
-                equalTo(healthInsurance.getYearlyFee()));
+        // create new contracts in database
+        ResponseEntity<Void> postResponseHealth = proxy.createEntity(healthInsurance);
+        ResponseEntity<Void> postResponseKfz = proxy.createEntity(kfz);
 
-        ResponseEntity<Contract> healthInsurance2FromDatabase =
-                getContract(locationToContract2.getHeaders().getLocation());
-        assertThat(healthInsurance2FromDatabase.getStatusCode(), equalTo(HttpStatus.OK));
-        // Compare local health insurance with health insurance from database, gained through get request
-        assertThat(healthInsurance2FromDatabase.getBody().getKindOfContract(),
-                equalTo(anotherHealthInsurance.getKindOfContract()));
-        assertThat(healthInsurance2FromDatabase.getBody().getYearlyFee(),
-                equalTo(anotherHealthInsurance.getYearlyFee()));
+        // Check if right status codes have been set
+        assertThat(postResponseHealth.getStatusCode(), equalTo(HttpStatus.CREATED));
+        assertThat(postResponseKfz.getStatusCode(), equalTo(HttpStatus.CREATED));
 
-        /**
-         * Delete created Contracts
-         */
-        deleteContract(locationToContract1.getHeaders().getLocation());
-        deleteContract(locationToContract2.getHeaders().getLocation());
-        /**
-         * Try to get Contracts from Database again
-         */
-        healthInsurance1FromDatabase =
-                getContract(locationToContract1.getHeaders().getLocation());
-        assertThat(healthInsurance1FromDatabase.getStatusCode(), equalTo(HttpStatus.NO_CONTENT));
+        // get created contracts from database
+        ResponseEntity<Contract> healtInsuranceFromDatabase =
+                proxy.getEntity(postResponseHealth.getHeaders().getLocation());
+        ResponseEntity<Contract> kfzFromDatabase =
+                proxy.getEntity(postResponseKfz.getHeaders().getLocation());
 
-        healthInsurance2FromDatabase =
-                getContract(locationToContract2.getHeaders().getLocation());
-        assertThat(healthInsurance2FromDatabase.getStatusCode(), equalTo(HttpStatus.NO_CONTENT));
+        // check if response contains right status codes
+        assertThat(healtInsuranceFromDatabase.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(kfzFromDatabase.getStatusCode(), equalTo(HttpStatus.OK));
+
+        // Compare local health insurance with health insurance from database
+        assert(healtInsuranceFromDatabase.getBody().equals(healthInsurance));
+        assert(kfzFromDatabase.getBody().equals(kfz));
+
+        // delete created contracts
+        proxy.deleteEntity(healtInsuranceFromDatabase.getBody().getId());
+        proxy.deleteEntity(kfzFromDatabase.getBody().getId());
+
+        // try to get deleted contracts
+        healtInsuranceFromDatabase =
+                proxy.getEntity(postResponseHealth.getHeaders().getLocation());
+        kfzFromDatabase =
+                proxy.getEntity(postResponseKfz.getHeaders().getLocation());
+
+        // check status
+        assertThat(healtInsuranceFromDatabase.getStatusCode(), equalTo(HttpStatus.NOT_FOUND));
+        assertThat(kfzFromDatabase.getStatusCode(), equalTo(HttpStatus.NOT_FOUND));
+
     }
 
 
 
-    private ResponseEntity<Void> createContract(Contract contract){
-        ResponseEntity<Void> response = restTemplate.postForEntity(REST_SERVICE_URI+"/contract",
-                contract, Void.class);
-        return response;
-    }
+    /**
+     * This test will simulate a situation, in which the lost update problem can occur, but the the first update wins
+     */
+    @Test
+    public void triggerLostUpdateProblem(){
+        // creates contract
+        ResponseEntity<Void> postResponse = proxy.createEntity(healthInsurance);
 
-    private ResponseEntity<Contract> getContract(URI location){
-        return restTemplate.getForEntity(location.toString(), Contract.class);
-    }
+        // Two clients gain the same contract
+        ResponseEntity<Contract> client1Response = proxy.getEntity(postResponse.getHeaders().getLocation());
+        Contract contract1 = client1Response.getBody();
 
-    private void updateContract(String id, Contract newContract){
-        // TODO restTemplate returned nichts ???????? Zumindest ResponseEntity wäre ganz nützlich!
-        restTemplate.put(REST_SERVICE_URI+"/contract/"+id, newContract, Contract.class);
-    }
+        ResponseEntity<Contract> client2Response = proxy.getEntity(postResponse.getHeaders().getLocation());
+        Contract contract2 = client2Response.getBody();
 
-    private void deleteContract(URI location){
-        // TODO implement
-        // TODO What to do, if user has this kind of contract ? Do not delete until no user has this contract anymore
-        // TODO add Status to contract -> Boolean deprecatedContract -> prevents more of these in Constructor
-        restTemplate.delete(location);
+        // The id of the resource is the same for both customers (same resource)
+        assertThat(contract1.getId(), equalTo(contract2.getId()));
+
+        Long resourceId = contract1.getId();
+
+        // Changes will be performed on the customers firstname
+        // Same situation as two clients get same customer and perform changes on resouce
+        contract1.setYearlyFee(new BigDecimal("1.10"));
+        contract2.setYearlyFee(new BigDecimal("99999999.2123"));
+
+        // Both clients try to change resource in database, but only the first update will actually be executed
+
+        // The version number of customer1 and the customer in the database are the same
+        // After the entity was updated, the version number of the resource got incremented
+        proxy.updateEntity(resourceId.toString(), contract1); // This update will be executed
+
+        // the version number of customer2 and the customer in the database are not the same
+        // because the database-customer's version number got incremented after first update
+        proxy.updateEntity(resourceId.toString(), contract2); // This update will not be executed
+
+        // Only the first update got executed so the name of the customer in the database is now Antonio
+        Contract updatedContract = proxy.getEntity(postResponse.getHeaders().getLocation()).getBody();
+        assertThat(updatedContract.getYearlyFee(), equalTo(new BigDecimal("1.10")));
     }
 }
