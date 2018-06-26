0bb0a145e9e60da79b9e5d5b4f920f76704b0edc
Denis Sabolotni <denissabolotnimail.com>
2018-06-26 20:47:44 +0200

    Timestamp can be returned as date or calender-instance now. Also you can get hour of date now

--- Assignment03/src/main/java/TelematicMessage.java
+++ Assignment03/src/main/java/TelematicMessage.java
@@ -1,6 +1,11 @@
-import java.io.Serializable;
+
+import com.google.gson.Gson;
+
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.UUID;
 
 public class TelematicMessage{
@@ -25,10 +30,63 @@ public class TelematicMessage{
      */
     Gps coordinates;
 
+    /**
+     * Can be used to convert date to string and vice versa
+     */
+    private DateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm.ss.SSS");
+
     public TelematicMessage(UUID telematicsId, Long drivenDistanceMeters, Gps coordinates) {
-        this.timestamp = new SimpleDateFormat("dd.MM.yyyy HH:mm.ss.SSS").format(System.currentTimeMillis());
+        this.timestamp = format.format(System.currentTimeMillis());
         this.telematicsId = telematicsId;
         this.drivenDistanceMeters = drivenDistanceMeters;
         this.coordinates = coordinates;
+    }
+
+    /**
+     * @returns used date format for messages
+     */
+    public DateFormat getFormat() {
+        return format;
+    }
+
+    /**
+     * @param json which is believed to be a serialzized telematic message
+     */
+    public static TelematicMessage deserialize(String json){
+        // easy json deserialize
+        Gson gson = new Gson();
+        return gson.fromJson(json, TelematicMessage.class);
+    }
+
+    /**
+     * Serializes instance to JSON
+     */
+    public String toJson(){
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    public Date getTimestampAsDate() {
+        Date date = null;
+        try {
+            date = format.parse(timestamp);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    public Calendar getTimestampAsCalender() {
+        Calendar cal = null;
+        try {
+            cal.setTime(getFormat().parse(timestamp));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return cal;
+    }
+
+    public Integer getHourOfDayOfCreation() {
+        return getTimestampAsCalender().get(Calendar.HOUR_OF_DAY);
     }
 }
